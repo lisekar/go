3a6d927a6287a9162f660d97d2be48cfc78fb065
lisekar <livingstansekar@gmail.com>
2025-11-22 15:42:24 +0530

    middleware concepts

--- http_server/main.go
+++ http_server/main.go
@@ -11,12 +11,12 @@ type Message struct {
 }
 
 func getUser1(w http.ResponseWriter, r *http.Request) {
-	msg := Message{Text: "Hello, User1!!!!!!!!!"}
+	msg := Message{Text: "Hello, User1"}
 	json.NewEncoder(w).Encode(msg)
 }
 
 func getUser2(w http.ResponseWriter, r *http.Request) {
-	msg := Message{Text: "Hello, User2!!!!"}
+	msg := Message{Text: "Hello, User2"}
 	json.NewEncoder(w).Encode(msg)
 }
 
@@ -24,8 +24,8 @@ func main() {
 	http.HandleFunc("/api/user1", getUser1)
 	http.HandleFunc("/api/user2", getUser2)
 	fmt.Println("Starting server at port 8000")
-	http.ListenAndServe(":8000", nil)
-	// if err := http.ListenAndServe(":8000", nil); err != nil {
-	// 	fmt.Println("Failed to start server:", err)
-	// }
+	// http.ListenAndServe(":8000", nil)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
